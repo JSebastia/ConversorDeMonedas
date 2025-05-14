135f444387a2a89973bc494b954bc6f1da3008a3
JSebastia <osoriosebastian748@gmail.com>
2025-05-14 13:07:27 -0500

    Se agrego la clase ConvertirJsonGson

--- src/com/Sebastian/ConversorDeMonedas/modelos/ConvertirJsonGson.java
+++ src/com/Sebastian/ConversorDeMonedas/modelos/ConvertirJsonGson.java
@@ -3,17 +3,21 @@ package com.Sebastian.ConversorDeMonedas.modelos;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class PasarJsonGson {
+public class ConvertirJsonGson {
     //ATRIBUTOS
     private String json;
     private MonedaExchangeRateAPI monedaExchangeRateAPI;
     private Gson gson;
 
     //METODOS
-    public PasarJsonGson(String json) {
+    public ConvertirJsonGson(String json) {
         this.json = json;
         this.gson = new GsonBuilder().create();
     }
 
-    public MonedaExchangeRateAPI pasarGson
+    public MonedaExchangeRateAPI convertirGson() {
+        //Se cambian los datos de json a tipo Titulo.
+        monedaExchangeRateAPI = gson.fromJson(json, MonedaExchangeRateAPI.class);
+        return monedaExchangeRateAPI;
+    }
 }
