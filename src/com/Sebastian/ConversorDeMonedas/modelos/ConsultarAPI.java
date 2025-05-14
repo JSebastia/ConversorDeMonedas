dea7d01c55733668282022c721283e1f311ba723
JSebastia <osoriosebastian748@gmail.com>
2025-05-13 23:06:57 -0500

    Se han creado nuevos archivos. Se creo la clase Principal y ConsultarAPI

---
+++ src/com/Sebastian/ConversorDeMonedas/modelos/ConsultarAPI.java
@@ -0,0 +1,33 @@
+package com.Sebastian.ConversorDeMonedas.modelos;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultarAPI {
+    //ATRIBUTOS
+    private String direccionURI;
+    private HttpClient client;
+    private HttpRequest request;
+    private HttpResponse<String> response;
+
+    //METDOS
+    public ConsultarAPI(String direccionURI) {//CONSTRUCTOR
+        this.direccionURI = direccionURI;
+        //HttpClient se encarga de enviar la solicitud al servidor.
+        client = HttpClient.newHttpClient();
+    }
+
+    public void pedirDatosAPI() {
+        //HttpRequest se encarga de realizar la petición al servidor con los detalles solicitados.
+        request = HttpRequest.newBuilder().uri(URI.create(direccionURI)).build();
+        //HttpRespose se encarga de recibir la respuesta de la petición (recibe los datos entregados por el servidor).
+        response = client.send(request,HttpResponse.BodyHandlers.ofString());
+    }
+
+    public String consultarDatos() {
+        //Retorna el Json de los datos.
+        return response.body();
+    }
+}
---
+++ src/com/Sebastian/ConversorDeMonedas/principal/Principal.java
@@ -0,0 +1,8 @@
+package com.Sebastian.ConversorDeMonedas.principal;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+
+}
