daa5db9619ceae023b968c3610c390e673221b73
JSebastia <osoriosebastian748@gmail.com>
2025-05-15 15:33:10 -0500

    Se han añadido algunas lineas

--- src/com/Sebastian/ConversorDeMonedas/modelos/ConsultarAPI.java
+++ src/com/Sebastian/ConversorDeMonedas/modelos/ConsultarAPI.java
@@ -21,10 +21,12 @@ public class ConsultarAPI {
     }
 
     public void pedirDatosAPI() throws IOException, InterruptedException {
+        System.out.println("Cargando...");//Muestra al usuario un "anuncio" (la consulta a la API puede tardar)
         //HttpRequest se encarga de realizar la petición al servidor con los detalles solicitados.
         request = HttpRequest.newBuilder().uri(URI.create(direccionURI)).build();
         //HttpRespose se encarga de recibir la respuesta de la petición (recibe los datos entregados por el servidor).
         response = client.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println("Completado.");//Mustra al usuario un "anuncio" (dar como terminada la consulta)
     }
 
     public String consultarDatos() {
